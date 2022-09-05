b9526046ef162b9fbe559197d61e16de36b25817
gustavo-gomess <gusgegomes@hotmail.com>
2022-09-05 13:38:43 -0300

    finalização

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username) 
+  
+  if(!user){
+    return response.status(404).json({error: "User already exist "})
+  } 
+
+  request.user = user 
+  
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if(!user.pro && user.todos < 10){
+    return next()
+  } else if (user.pro){
+    return next()
+  }else{
+    return response.status(403).json({ error: "limit reached" })
+  }
+
+  return next() 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).json({ error: "user not found" });
+
+  const checkUuidIsValid = validate(id);
+  if (!checkUuidIsValid)
+    return response.status(400).json({ error: "Id invalid" });
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) return response.status(404).json({ error: "todo not found" });
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
+
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const checkUuidIsValid = validate(id);
+
+  if (!checkUuidIsValid) {
+    return response.status(400).json({ error: "Id invalid" });
+  }
+
+  if (!id) return response.status(400).json({ error: "id is required" });
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  } else {
+    request.user = user;
+  }
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -127,4 +182,4 @@ module.exports = {
   checksCreateTodosUserAvailability,
   checksTodoExists,
   findUserById
-};
+}
